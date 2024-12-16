4e1d09f5befa7442353abd56aee67cb1165e3469
nidanurefe <efen22@itu.edu.tr>
2024-12-17 00:09:26 +0300

    fetch from tmdb updated

--- insert_from_tmdb.py
+++ insert_from_tmdb.py
@@ -11,7 +11,7 @@ API_KEY = os.getenv('API_KEY')
 BASE_URL = 'https://api.themoviedb.org/3'
 IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500"
 
-def get_db_connection(app):
+def get_db_connection(app)->pymysql.Connection:
     return pymysql.connect(
         host=app.config['MYSQL_HOST'],
         user=app.config['MYSQL_USER'],
@@ -23,18 +23,18 @@ def get_db_connection(app):
 
 
 def get_movies(page=1)->dict:
-    url = f"{BASE_URL}/discover/movie"
-    headers = {
+    url:str = f"{BASE_URL}/discover/movie"
+    headers:list = {
         "Authorization": f"Bearer {API_KEY}",
         "accept": "application/json"
     }
-    params = {
+    params:list = {
         "include_adult": "false",
         "language": "en-US",
         "page": page,
         "sort_by": "popularity.desc"
     }
-    response = requests.get(url, headers=headers, params=params)
+    response:dict = requests.get(url, headers=headers, params=params)
     if response.status_code == 200:
         return response.json()
     else:
@@ -42,7 +42,7 @@ def get_movies(page=1)->dict:
         return None
 
 def get_movie_cast(movie_id)->list:
-    url = f"{BASE_URL}/movie/{movie_id}/credits"
+    url:str= f"{BASE_URL}/movie/{movie_id}/credits"
     headers = {
         "Authorization": f"Bearer {API_KEY}",
         "accept": "application/json"
@@ -55,12 +55,12 @@ def get_movie_cast(movie_id)->list:
         return []
 
 def get_genres()->list:
-    url = f"{BASE_URL}/genre/movie/list"
-    headers = {
+    url:str = f"{BASE_URL}/genre/movie/list"
+    headers:list = {
         "Authorization": f"Bearer {API_KEY}",
         "accept": "application/json"
     }
-    response = requests.get(url, headers=headers)
+    response:dict = requests.get(url, headers=headers)
     if response.status_code == 200:
         return response.json().get('genres', [])
     else:
@@ -70,12 +70,12 @@ def get_genres()->list:
 
 
 def get_movie_details(movie_id)->dict:
-    url = f"{BASE_URL}/movie/{movie_id}"
-    headers = {
+    url:str = f"{BASE_URL}/movie/{movie_id}"
+    headers:list = {
         "Authorization": f"Bearer {API_KEY}",
         "accept": "application/json"
     }
-    response = requests.get(url, headers=headers)
+    response:dict = requests.get(url, headers=headers)
     if response.status_code == 200:
         return response.json()
     else:
@@ -84,8 +84,8 @@ def get_movie_details(movie_id)->dict:
 
 def insert_movie(connection, movie) -> None:
     with connection.cursor() as cursor:  
-        genre_id = movie['genre_ids'][0] if movie['genre_ids'] else None
-        movie_details = get_movie_details(movie['id'])
+        genre_id:int = movie['genre_ids'][0] if movie['genre_ids'] else None
+        movie_details:dict = get_movie_details(movie['id'])
         if movie_details:
             producers = movie_details.get('production_companies', [])
             if producers:
@@ -93,13 +93,13 @@ def insert_movie(connection, movie) -> None:
                 producer_name = producers[0]['name']
                 insert_producer(connection, producer_id, producer_name)
         print(movie_details['production_companies'][0])
-        poster_path = IMAGE_BASE_URL + movie.get('poster_path', '')
+        poster_path:str = IMAGE_BASE_URL + movie.get('poster_path', '')
 
         check_sql = "SELECT COUNT(*) AS count FROM movies WHERE tmdb_id = %s"
         cursor.execute(check_sql, (movie['id'],))
-        result = cursor.fetchone()
+        result:dict = cursor.fetchone()
 
-        count = result['count'] if isinstance(result, dict) else result[0]
+        count:int = result['count'] if isinstance(result, dict) else result[0]
 
         if count == 0:  
             sql = """
@@ -125,7 +125,7 @@ def insert_movie(connection, movie) -> None:
 def insert_producer(connection, producer_id, producer_name)->None:
     with connection.cursor() as cursor:
         cursor.execute("SELECT id FROM producers WHERE id = %s", (producer_id,))
-        result = cursor.fetchone()
+        result:dict = cursor.fetchone()
         
         if not result:  
             sql = """
@@ -139,14 +139,14 @@ def insert_producer(connection, producer_id, producer_name)->None:
 
 
 def insert_actor(connection, actor_id, actor_name, img_url=None) -> None:
-    img_url = IMAGE_BASE_URL + img_url if img_url else None
+    img_url:str= IMAGE_BASE_URL + img_url if img_url else None
 
     with connection.cursor() as cursor:
         check_sql = "SELECT COUNT(*) AS count FROM actors WHERE id = %s"
         cursor.execute(check_sql, (actor_id,))
-        result = cursor.fetchone()
+        result:dict = cursor.fetchone()
 
-        count = result['count'] if isinstance(result, dict) else result[0]
+        count:int = result['count'] if isinstance(result, dict) else result[0]
 
         if count == 0:
             sql = """
@@ -181,9 +181,9 @@ def insert_movie_actor(connection, movie_id, actor_id) -> None:
             WHERE movie_id = %s AND actor_id = %s
             """
             cursor.execute(check_sql, (movie_id, actor_id))
-            result = cursor.fetchone()
+            result:dict = cursor.fetchone()
 
-            count = result['count'] if isinstance(result, dict) else result[0]
+            count:int = result['count'] if isinstance(result, dict) else result[0]
 
             if count == 0:
                 sql = """
@@ -201,51 +201,43 @@ def insert_movie_actor(connection, movie_id, actor_id) -> None:
 
 
 def link_movie_producers(connection, movie_id)->None:
-    movie_details = get_movie_details(movie_id)
+    movie_details:dict = get_movie_details(movie_id)
     if not movie_details:
         return
-    producers = movie_details.get('production_companies', [])
+    producers:dict = movie_details.get('production_companies', [])
     for producer in producers:
-        producer_id = producer['id']
-        producer_name = producer['name']
-        
+        producer_id:int = producer['id']
+        producer_name:int = producer['name']
         insert_producer(connection, producer_id, producer_name)
 
 
 def insert_all_genres(connection)->None:
-    genres = get_genres()
-    print(genres)
+    genres:dict = get_genres()
     for genre in genres:
         insert_genre(connection, genre['id'], genre['name'])
 
 def main():
-    connection = get_db_connection(app) 
-    print("Adding genres...")
+    connection:pymysql.Connection = get_db_connection(app)
     insert_all_genres(connection)
-
-    print("Fetching movies...")
-    movies_data = get_movies(page=1)
+    movies_data:dict = get_movies(page=1)
       
-
     if movies_data:
-        print("Adding producers...")
         for movie in movies_data['results']:
             try:
                 link_movie_producers(connection, movie['id'])
             except Exception as e:
                 print(f"Error adding producers for movie {movie['id']}: {e}")
 
-            print("Adding movies and actors...")
             for movie in movies_data['results']:
                 insert_movie(connection, movie)
             
             for movie in movies_data['results']:
 
-                cast = get_movie_cast(movie['id'])
+                cast:dict = get_movie_cast(movie['id'])
                 for actor in cast:
-                    actor_id = actor.get('id')
-                    actor_name = actor.get('name')
-                    img_url = actor.get('profile_path', None)
+                    actor_id:int = actor.get('id')
+                    actor_name:int = actor.get('name')
+                    img_url:str = actor.get('profile_path', None)
 
                     if actor_id and actor_name:
                         insert_actor(connection, actor_id, actor_name, img_url)
